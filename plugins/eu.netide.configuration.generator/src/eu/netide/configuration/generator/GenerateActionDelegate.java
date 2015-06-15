eabac78637e8c1a24820d56cfd4a042d330a3364
Christian Stritzke <christian.stritzke@ipt.fraunhofer.de>
2015-06-16 01:08:58 +0200

    First working maven configuration

--- plugins/eu.netide.configuration.generator/src/eu/netide/configuration/generator/GenerateActionDelegate.java
+++ plugins/eu.netide.configuration.generator/src/eu/netide/configuration/generator/GenerateActionDelegate.java
@@ -5,6 +5,7 @@ import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.ui.IObjectActionDelegate;
 import org.eclipse.ui.IWorkbenchPart;
+import eu.netide.configuration.generator.CommonConfigurationModule;
 
 import Topology.NetworkEnvironment;
 
