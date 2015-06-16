10d13d6fc5b140ab96a783f44d74f7fa98fea6d9
antonis-m <antonismanousis@gmail.com>
2015-06-16 14:09:54 +0100

    registered network elements according to input file

--- plugins/eu.netide.deployment.topologyimport/src/eu/netide/deployment/topologyimport/TopologyImport.java
+++ plugins/eu.netide.deployment.topologyimport/src/eu/netide/deployment/topologyimport/TopologyImport.java
@@ -5,6 +5,7 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.HashMap;
 
 
 /*import java.lang.reflect.InvocationTargetException;
@@ -29,18 +30,6 @@ public class TopologyImport {
 		
 		File file = new File(args[0]);
 		BufferedReader reader = null;
-		try {
-		    reader = new BufferedReader(new FileReader(file));
-		    String text = null;
-
-		    while ((text = reader.readLine()) != null) {
-		        //Do stuff - read lines
-		    }
-		} catch (FileNotFoundException e) {
-		    e.printStackTrace();
-		} catch (IOException e) {
-		    e.printStackTrace();
-		}
 		
 		TopologyFactory factory = TopologyFactory.eINSTANCE;
 		NetworkEnvironment ne = factory.createNetworkEnvironment();
@@ -49,6 +38,53 @@ public class TopologyImport {
 		Network net = factory.createNetwork();
 		net.setNetworkenvironment(ne);
 		net.setName("TestNet");
+		
+		try {
+		    reader = new BufferedReader(new FileReader(file));
+		    String text = null;
+		    int nr_switches = Integer.parseInt(reader.readLine());
+		    String base_str = "sw";
+		    HashMap<String, Switch> sw_list = new HashMap<String, Switch>();
+		    for (int i=0 ; i < nr_switches; i++) {
+		    	text = reader.readLine();
+		    	String name = base_str + "_" +String.valueOf(i);
+		    	Switch sw = factory.createSwitch();
+		    	sw.setName(name);
+		    	sw.setTopology(net);
+		    	sw_list.put(name, sw);
+		    }
+		    int nr_edges = Integer.parseInt(reader.readLine());
+		    HashMap<String, Connector> connector_list = new HashMap<String, Connector>();
+		    HashMap<Connector, Port[]> port_list = new HashMap<Connector, Port[]>();
+		    for (int i=0 ; i < nr_edges; i++) {
+		    	text = reader.readLine();
+		    	String[] edges = new String[2] ;
+		    	Port[] ports = new Port[2];
+		    	edges = text.split("   ");
+		    	String name =  "edge_" +String.valueOf(i);
+		    	//System.out.println(edges[0]+" "+edges[1]);
+		    	Connector con = factory.createConnector();
+				con.setNetwork(net);
+				connector_list.put(name, con);
+				Port port1= factory.createPort();
+				port1.setConnector(con);
+				Port port2 = factory.createPort();
+				port2.setConnector(con);
+				ports[0] = port1;
+				ports[1] = port2;
+				// port1.setNetworkelement(ne); WHICH Network Environment ?
+				port_list.put(con, ports);
+		    }
+		    while ((text = reader.readLine()) != null) {
+		    	System.out.println(text);
+		    }
+		} catch (FileNotFoundException e) {
+		    e.printStackTrace();
+		} catch (IOException e) {
+		    e.printStackTrace();
+		} 
+		
+
 		
 		Switch sw = factory.createSwitch();
 		sw.setName("TestSwitch");
