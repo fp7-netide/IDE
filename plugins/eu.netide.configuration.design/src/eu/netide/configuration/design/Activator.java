72544b8ae357084e7d5cac918152b5ce11febfb6
Christian Stritzke <christian.stritzke@ipt.fraunhofer.de>
2015-07-06 13:35:55 +0200

    Network.design to eu.netide.

--- plugins/eu.netide.configuration.design/src/eu/netide/configuration/design/Activator.java
+++ plugins/eu.netide.configuration.design/src/eu/netide/configuration/design/Activator.java
@@ -1,4 +1,4 @@
-package Network.design;
+package eu.netide.configuration.design;
 
 import java.util.HashSet;
 import java.util.Set;
@@ -13,7 +13,7 @@ import org.osgi.framework.BundleContext;
  */
 public class Activator extends AbstractUIPlugin {
     // The plug-in ID
-    public static final String PLUGIN_ID = "Network.design";
+    public static final String PLUGIN_ID = "eu.netide.configuration.design";
 
     // The shared instance
     private static Activator plugin;
@@ -35,7 +35,7 @@ public class Activator extends AbstractUIPlugin {
       super.start(context);
 	  plugin = this;
 	  viewpoints = new HashSet<Viewpoint>();
-	  viewpoints.addAll(ViewpointRegistry.getInstance().registerFromPlugin(PLUGIN_ID + "/description/My.odesign")); 
+	  viewpoints.addAll(ViewpointRegistry.getInstance().registerFromPlugin(PLUGIN_ID + "/description/Configuration.odesign")); 
     }
 
     /*
