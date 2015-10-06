e15b66e3f3abf58ff1a75fc1f5de674866be47c1
Christian Stritzke <christian.stritzke@ipt.fraunhofer.de>
2015-10-06 10:41:21 +0200

    Nemo Language editors

---
+++ plugins/eu.netide.telefonica.nemo.ui/src/eu/netide/telefonica/nemo/ui/NemoDSLHighlightingCalculator.java
@@ -0,0 +1,17 @@
+package eu.netide.telefonica.nemo.ui;
+
+import org.eclipse.xtext.resource.XtextResource;
+import org.eclipse.xtext.ui.editor.syntaxcoloring.IHighlightedPositionAcceptor;
+import org.eclipse.xtext.ui.editor.syntaxcoloring.ISemanticHighlightingCalculator;
+
+public class NemoDSLHighlightingCalculator implements
+		ISemanticHighlightingCalculator {
+
+	@Override
+	public void provideHighlightingFor(XtextResource resource,
+			IHighlightedPositionAcceptor acceptor) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
