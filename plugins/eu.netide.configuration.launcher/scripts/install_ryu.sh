f1e5383f2479df2d0a2f46d2fa2d11255c710a44
Gregor Best <gbe@mail.upb.de>
2015-10-07 15:45:45 +0200

    Clean up Ryu installation script
    Also installs Python 0MQ bindings
    
    Signed-off-by: Gregor Best <gbe@mail.upb.de>

--- plugins/eu.netide.configuration.launcher/scripts/install_ryu.sh
+++ plugins/eu.netide.configuration.launcher/scripts/install_ryu.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 if [ "$(which pip)" == "" ] || [ "$(pip list | grep ryu)" == "" ]; then
 
-  sudo apt-get --yes install python-pip python-dev python-repoze.lru libxml2-dev libxslt1-dev zlib1g-dev
+  sudo apt-get --yes install python-pip python-dev python-repoze.lru libxml2-dev libxslt1-dev zlib1g-dev python-zmq python3-zmq
   sudo -E pip install ecdsa
   sudo -E pip install stevedore
   sudo -E pip install greenlet
   sudo -E pip install ryu
   
-  sudo cp -r Engine/ryu-backend/netide /usr/local/lib/python2.7/dist-packages/ryu/
-  sudo cp -r Engine/ryu-shim/netide /usr/local/lib/python2.7/dist-packages/ryu/
 
-  mkdir -p ryu/ryu
-  cp -r Engine/ryu-backend/netide ryu/ryu
-  cp -r Engine/ryu-shim/netide ryu/ryu
+  # sudo cp -r Engine/ryu-backend/netide /usr/local/lib/python2.7/dist-packages/ryu/
+  # sudo cp -r Engine/ryu-shim/netide /usr/local/lib/python2.7/dist-packages/ryu/
+
+  # mkdir -p ryu/ryu
+  # cp -r Engine/ryu-backend/netide ryu/ryu
+  # cp -r Engine/ryu-shim/netide ryu/ryu
 
   #git clone git://github.com/osrg/ryu.git
   #cd ryu
