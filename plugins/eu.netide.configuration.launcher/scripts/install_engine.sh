de10b8a28910bda1f1324dac053278f84e83c754
Gregor Best <gbe@mail.upb.de>
2015-10-07 15:39:20 +0200

    Add path to netip libraries to Python path when installing engine
    This doesn't exactly feels like the right way to do this, but gets the
    job done. We should probably convert the Python library to a
    PIP-installable Python package
    
    Signed-off-by: Gregor Best <gbe@mail.upb.de>

--- plugins/eu.netide.configuration.launcher/scripts/install_engine.sh
+++ plugins/eu.netide.configuration.launcher/scripts/install_engine.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
+
 cd
 
 if [ ! -d ~/Engine ]; then
   git clone https://github.com/fp7-netide/Engine
 fi
 
-# TODO: install netip libraries so that java and python can find them
+x="export PYTHONPATH=\$PYTHONPATH:~/Engine/libraries/netip/python"
 
-#echo "export PYTHONPATH=\$PYTHONPATH:~/Engine/ryu-backend:~/Engine/ryu-backend/tests" >> $HOME/.bashrc
-#echo "export PYTHONPATH=\$PYTHONPATH:~/Engine/ryu-backend:~/Engine/ryu-backend/tests" >> $HOME/.profile
+echo "$x" >> $HOME/.bashrc
+echo "$x" >> $HOME/.profile
 
-#echo "export PYTHONPATH=\$PYTHONPATH:~/Engine/ryu-shim" >> $HOME/.bashrc
-#echo "export PYTHONPATH=\$PYTHONPATH:~/Engine/ryu-shim" >> $HOME/.profile
+# TODO: install netip libraries so that java can find them
