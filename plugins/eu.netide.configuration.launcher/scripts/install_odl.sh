516108fee25603042b008a397a5962c04b015a1b
Christian Stritzke <christian.stritzke@ipt.fraunhofer.de>
2015-10-26 16:05:50 +0100

    Install ODL bundles during VM setup

--- plugins/eu.netide.configuration.launcher/scripts/install_odl.sh
+++ plugins/eu.netide.configuration.launcher/scripts/install_odl.sh
@@ -12,6 +12,23 @@ if [ ! -d ~/openflowplugin ]; then
    mv pom.xml pom_other_release.xml
    mv pom_sr1.xml pom.xml
    mvn clean install
+
+   cd
+   cd ./openflowplugin/distribution/karaf/
+   mvn clean package
+   cd ./target/assembly/bin
+
+   chmod +x ./client ./start ./stop
+   echo "Installing karaf dependencies for ODL shim"
+   ./start
+   sleep 10
+   ./client "bundle:install -s mvn:com.googlecode.json-simple/json-simple/1.1.1"
+   ./client "bundle:install -s mvn:org.apache.commons/commons-lang3/3.3.2"
+   ./client "bundle:install -s mvn:org.opendaylight.openflowplugin/pyretic-odl/0.0.4-Helium-SR1.1"
+   sleep 10
+   ./stop
+
+   cd 
    
 else
 
